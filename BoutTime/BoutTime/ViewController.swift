20c3c0011c4bc0ba12521f5db3e59217e0e781b6
David laiymani <david.laiymani@gmail.com>
2019-03-24 08:37:36 +0100

    ready to submit

--- BoutTime/BoutTime/ViewController.swift
+++ BoutTime/BoutTime/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
     @IBOutlet var downButtons: [UIButton]!
     @IBOutlet weak var nextRoundButton: UIButton!
     @IBOutlet weak var timerLabel: UILabel!
-    @IBOutlet weak var infoLabel: UILabel!
+    @IBOutlet weak var infoLabel: UILabel! 
     
     var timer = Timer()
     let gameManager = GameManager(numberOfRounds: 6, eventsPerRound: 4, secondsPerRound: 60)
